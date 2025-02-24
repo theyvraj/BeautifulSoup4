cf3153367086c212eed1b5df0e51531afba04059
Devraj Singh <aol.devraj@aol.com>
2025-02-24 11:50:10 +0530

    modified:   02_tags.py

--- 02_tags.py
+++ 02_tags.py
@@ -9,9 +9,20 @@ tag = soup.b
 #print(tag.name)
 #print(tag.attrs)
 tag.name = 'g'
-print(tag.name)
-tag['class'] = "container"
+# print(tag.name)
+tag['class'] = 'container'
 print(tag.get_attribute_list('class'))
-new_tag = soup.find_all('b')
+new_tag = soup.find_all('g')
+# for i in new_tag:
+#     print(i.get_attribute_list('class'))
+tag['class'] = ['container', 'test_add']
+tag['id'] = 'test_id'
+print(tag)
 for i in new_tag:
     print(i.get_attribute_list('class'))
+    print(i.get('id'))
+    
+# DELETING ATTRIBUTE
+
+del tag['id']
+print(tag.get('id'))
