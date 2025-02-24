7799f3286acca30e3102763aa47b47764ddbec1d
Devraj Singh <aol.devraj@aol.com>
2025-02-24 10:45:10 +0530

    modified:   02_tags.py

--- 02_tags.py
+++ 02_tags.py
@@ -1,2 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+
+soup = BeautifulSoup('<b class="boldest">Extremely bold</b>', 'html.parser')
+
+# TAG CREATION
+
+tag = soup.b
+#print(tag.name)
+#print(tag.attrs)
+tag.name = 'g'
+print(tag.name)
+tag['class'] = "container"
+print(tag.get_attribute_list('class'))
